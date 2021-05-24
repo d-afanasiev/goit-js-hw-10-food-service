4af242e850fad27f8df3d8a29d7b443dce7c951f
d-afanasiev <59284294+d-afanasiev@users.noreply.github.com>
2021-05-24 15:03:04 +0000

    Deploying to gh-pages from @ d-afanasiev/goit-js-hw-10-food-service@fc47855dfb9f7cd227298804a03ba6547477db68 🚀

--- src.b68642ce.js
+++ src.b68642ce.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
+},{}],"qwQt":[function(require,module,exports) {
+const e={LIGHT:"light-theme",DARK:"dark-theme"},t="theme",c="chekbox",o={themeSwitch:document.querySelector("#theme-switch-toggle")};document.body.classList.add(localStorage.getItem("theme")),localStorage.getItem("chekbox")&&(o.themeSwitch.checked=localStorage.getItem("chekbox")),o.themeSwitch.addEventListener("change",t=>{if(document.body.classList.contains(e.DARK))return localStorage.setItem(["theme"],e.LIGHT),localStorage.setItem(["chekbox"],o.themeSwitch.checked),document.body.classList.remove(`${e.DARK}`),void document.body.classList.add(`${e.LIGHT}`);localStorage.setItem(["theme"],e.DARK),localStorage.setItem(["chekbox"],o.themeSwitch.checked),document.body.classList.add(`${e.DARK}`),document.body.classList.remove(`${e.LIGHT}`)});
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10-food-service/src.41377dc2.js.map
+"use strict";require("./sass/main.scss"),require("./js/theme.js");
+},{"./sass/main.scss":"clu1","./js/theme.js":"qwQt"}]},{},["Focm"], null)
+//# sourceMappingURL=/goit-js-hw-10-food-service/src.b68642ce.js.map
