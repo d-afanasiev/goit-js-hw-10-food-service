1fdeb17ca138aad918b29d403a3e1ebf4359ceba
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-05-25 11:39:50 +0300

    fix cheked theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -10,21 +10,21 @@ const refs = {
   themeSwitch: document.querySelector('#theme-switch-toggle'),
 };
 
-document.body.classList.add(localStorage.getItem(STORAGE_THEME));
-if (localStorage.getItem(STORAGE_STATUS)) {
+if (localStorage.getItem(STORAGE_STATUS) === 'true') {
   refs.themeSwitch.checked = localStorage.getItem(STORAGE_STATUS);
+  document.body.classList.add(localStorage.getItem(STORAGE_THEME));
 }
 
 refs.themeSwitch.addEventListener('change', event => {
-  if (document.body.classList.contains(Theme.DARK)) {
-    localStorage.setItem([STORAGE_THEME], Theme.LIGHT);
+  if (refs.themeSwitch.checked) {
+    localStorage.setItem([STORAGE_THEME], Theme.DARK);
     localStorage.setItem([STORAGE_STATUS], refs.themeSwitch.checked);
-    document.body.classList.remove(`${Theme.DARK}`);
-    document.body.classList.add(`${Theme.LIGHT}`);
+    document.body.classList.remove(`${Theme.LIGHT}`);
+    document.body.classList.add(`${Theme.DARK}`);
     return;
   }
-  localStorage.setItem([STORAGE_THEME], Theme.DARK);
+  localStorage.setItem([STORAGE_THEME], Theme.LIGHT);
   localStorage.setItem([STORAGE_STATUS], refs.themeSwitch.checked);
-  document.body.classList.add(`${Theme.DARK}`);
-  document.body.classList.remove(`${Theme.LIGHT}`);
+  document.body.classList.add(`${Theme.LIGHT}`);
+  document.body.classList.remove(`${Theme.DARK}`);
 });
